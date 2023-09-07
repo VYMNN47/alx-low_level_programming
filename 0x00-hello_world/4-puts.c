0ec89703b1d28ed048cdd48137c13d44992faf61
aymnn <ayman.belahsen.4@gmail.com>
2023-09-07 15:17:18 -0700

    using betty for the first time

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 /**
- * main - Entery point
- *
- * Description: A C program that prints a sentence
+ * main - A C program that prints a sentence
  *
  * Return: Always 0 (Success)
  *
