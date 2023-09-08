b2c2b661eb6efafeb1304946d4a5a8300d572a9b
aymnn <ayman.belahsen.4@gmail.com>
2023-09-08 13:46:51 -0700

    printing numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * A C program that prints all single digit numbers of base 10
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	int digit = 0;
+
+		while (digit <= 0)
+		{
+			printf("%d", digit);
+			digit++;
+		}
+		printf("\n");
+
+		return (0);
+}
+
