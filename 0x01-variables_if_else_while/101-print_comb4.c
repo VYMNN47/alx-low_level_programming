3f6231a217b85ec608fb7be17a2c3657f242d187
aymnn <ayman.belahsen.4@gmail.com>
2023-09-09 10:43:41 -0700

    r

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * A C program that prints all possible different combinations of three digits
+ *
+ * Return: 0 (success)
+*/
 
 int main(void)
 {
@@ -14,9 +21,9 @@ int main(void)
 			while (num3 <= 9)
 			{
 				if (num1 != num2 &&
-			           num1 < num2 &&	
-				   num2 != num3 &&
-				   num2 < num3)
+			            num1 < num2 &&
+				    num2 != num3 &&
+				    num2 < num3)
 				{
 					putchar(num1 + '0');
 					putchar(num2 + '0');
@@ -32,7 +39,7 @@ int main(void)
 			}
 			num2++;
 		}
-		num1++
+		num1++;
 	}
 	putchar('\n');
 
