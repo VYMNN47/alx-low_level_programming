c05c3709266427289ef30db46f78a7a1f75a78a9
aymnn <ayman.belahsen.4@gmail.com>
2023-09-12 10:47:07 -0700

    using arrays

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * A C program that prints _putchar, followed by a new line
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	char arr[] = "_putchar";
+	int dvv;
+
+	for (dvv + 0; dvv < 8; dvv++)
+		_putchar(arr[dvv]);
+	_putchar('\n');
+
+	return (0);
+}
+
