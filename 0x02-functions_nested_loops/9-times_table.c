efb9930289335bc6f5c840d619534ecad34acb39
aymnn <ayman.belahsen.4@gmail.com>
2023-09-12 13:45:50 -0700

    r

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,14 +16,14 @@ void times_table(void)
 			_putchar(',');
 			_putchar(' ');
 
-			prod = num * mult;
+			prob = num * mult;
 
 			if (prob <= 9)
 				_putchar(' ');
 			else
-				_putchar((prod / 10) + 48);
+				_putchar((prob / 10) + 48);
 
-			_putchar((prod % 10) + 48);
+			_putchar((prob % 10) + 48);
 		}
 		putchar('\n');
 	}
