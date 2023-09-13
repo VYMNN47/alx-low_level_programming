f49aac797fbf4aaa6cf5c4404d5cae9c3e6107d6
aymnn <ayman.belahsen.4@gmail.com>
2023-09-13 07:23:33 -0700

    t

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * A C program that prints the sum of evne-valued
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	unsigned long fibo1 = 0, fibo2 = 1, sum;
+	double total_sum;
+
+	while (1)
+	{
+		sum = fibo1 + fibo2;
+
+		if ((sum % 2) == 0)
+			total_sum += sum;
+
+		fibo1 = fibo2;
+		fibo2 = sum;
+	}
+	printf("%.0f\n", total_sum);
+
+	return (0);
+}
+
