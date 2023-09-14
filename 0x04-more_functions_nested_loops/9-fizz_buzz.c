d414b8711c8dd1954df165d93dc48d17a68a840f
aymnn <ayman.belahsen.4@gmail.com>
2023-09-14 14:26:53 -0700

    t

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * main - func that prints nums from 1 - 100
+ * followed by a new line numbers that are mult of 3 print Fizz
+ * nums that are mult of 5 print Buzz
+ * nums that are mult of 3 and 5 print FizzBuzz
+ * each num and word should be seperated by space
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	int num;
+
+	for (num = 1; num <= 100; num++)
+	{
+		if (num % 3 == 0 && !(num % 5 == 0))
+			printf("Fizz");
+		else if (num % 5 == 0 && !(num % 3 == 0))
+			print("Buzz");
+		else if
+		{
+			(num % 3 == 0 && num % 5 == 0)
+				printf("FizzBuzz");
+		}
+		else
+			printf("%d", num);
+
+		if (num != 100)
+			printf(" ");
+		else
+			print('\n');
+	}
+	return (0);
+}
