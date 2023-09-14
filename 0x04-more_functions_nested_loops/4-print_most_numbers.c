332c8d8bcac2ec76604efe04601da17cc80709f1
aymnn <ayman.belahsen.4@gmail.com>
2023-09-14 10:10:01 -0700

    r

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,13 +9,13 @@
 
 void print_most_numbers(void)
 {
-        int digit;
+	int digit;
 
-        for (digit = 0; digit <= 9; digit++)
-        {
-                if (digit == 2 || digit == 4)
-                        continue;
-                _putchar(digit + '0');
-        }
-        _putchar('\n');
+	for (digit = 0; digit <= 9; digit++)
+	{
+		if (digit == 2 || digit == 4)
+			continue;
+		_putchar(digit + '0');
+	}
+	_putchar('\n');
 }
