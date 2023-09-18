d2328d0a8bd0ebd4c5dfc46537335c6c4f4afe9e
aymnn <ayman.belahsen.4@gmail.com>
2023-09-18 13:51:44 -0700

    t

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - a function that prints a string
+ *
+ * @str : string to print
+ *
+ * Return: Nothing
+*/
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str + 0);
+		str++;
+	}
+	_putchar('\n');
+}
