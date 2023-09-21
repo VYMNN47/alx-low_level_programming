1335f306730e2966b91cefa00ea0062cc9d2c1ee
aymnn <ayman.belahsen.4@gmail.com>
2023-09-21 10:24:46 -0700

    r

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * islower - a function that capitalizes all words of a string.
+ *
+ * @c: char
+ *
+ * Return: 1 if true, 0 if false
+*/
+
+int isLower
