4b5e9f0081a38f620a496cebc892045964bdcb2b
aymnn <ayman.belahsen.4@gmail.com>
2023-09-25 16:00:19 -0700

    r

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_chessboard - a function that prints the chessboard
+ *
+ * @a: rows
+ *
+ * Return: nothing
+*/
+
+void print_chessboard(char (*a)[8])
+{
+	int ert, lo;
+
+	for (ert = 0; ert < 8; ert++)
+	{
+		for (lo = 0; lo < 8; lo++)
+		{
+			_putchar(a[ert][lo]);
+		}
+		_putchar('\n');
+	}
+}
