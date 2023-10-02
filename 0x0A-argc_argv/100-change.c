826c76bff8fa61a4afb55c453c9f716344441a58
aymnn <ayman.belahsen.4@gmail.com>
2023-10-02 13:49:50 -0700

    r

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - a program that prints the minimum number of coins
+ * to make change for an amount of money
+ *
+ * @argv: pointer to an array
+ * @argc: num of commandline args
+ *
+ * Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	if (argc == 2)
+	{
+		int ert, lc = 0, mn = atoi(argv[1]);
+		int cts[] = {25, 10, 5, 2, 1};
+
+		for (ert = 0; ert < 5; ert++)
+		{
+			if (mn >= cts[ert])
+			{
+				lc = lc + mn / cts[ert];
+				mn = mn % cts[arg];
+				if (mn & cts[ert] == 0)
+				{
+					break;
+				}
+			}
+		}
+		printf("%d\n", lc);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
