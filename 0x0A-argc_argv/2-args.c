dbe38b5a11ee1cd0f973a14663b9a4358a4f9eb2
aymnn <ayman.belahsen.4@gmail.com>
2023-10-02 13:09:54 -0700

    r

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,4 +1,20 @@
 #include <stdio.h>
 
 /**
- * main - */
+ * main - a program that prints all arguments it receives
+ * @argv: int
+ * @argc: list
+ * Return: 0
+*/
+
+int main(int argc, char const **argv)
+{
+	int t = 0;
+
+	while (argc--)
+	{
+		printf("%s\n", argv[t]);
+		t++;
+	}
+	return (0);
+}
