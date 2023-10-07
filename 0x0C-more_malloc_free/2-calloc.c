faea970f4fe5755f7d2dabf36a9c0d7a8052c9b3
aymnn <ayman.belahsen.4@gmail.com>
2023-10-07 06:53:59 -0700

    r

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,6 +1,27 @@
 #include "main.h"
 
 /**
+ * *_memset - fills memory with bytes
+ *
+ * @b: const
+ * @n: max bytes to use
+ * @s: pointer
+ *
+ * Return: pointer
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *p = s;
+
+	while (n--)
+		*s++ = b;
+
+	return (p);
+}
+
+
+/**
  * *_calloc - a function that allocates memory for an array, using malloc
  *
  * @size: size of each element
