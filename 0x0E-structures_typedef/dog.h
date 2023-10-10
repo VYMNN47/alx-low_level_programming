23986ad1770b2325f051db82c7999e065bf7050f
aymnn <ayman.belahsen.4@gmail.com>
2023-10-10 16:47:09 -0700

    Sturcts are little fun

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a struct for some dog elements
+ *
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: the name of the dog's owner
+ *
+ * Description: just some dog info
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
