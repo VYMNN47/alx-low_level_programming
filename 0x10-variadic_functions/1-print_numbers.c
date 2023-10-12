9ee3cfff3228ec127324a3d359a36cfaea538d18
aymnn <ayman.belahsen.4@gmail.com>
2023-10-12 16:45:43 -0700

    R

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - a function that prints numbers
+ *
+ * @seperator: string separator
+ * @n: num of args
+ * @...: ints to print
+ *
+ * Return: void
+*/
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	int t = n;
+	va_list ert;
+
+	if (!n)
+	{
+		printf("\n");
+		return;
+	}
+	va_start(ert, n);
+	while (t--)
+		printf("%d%s", va_arg(ert, int),
+				t ? (separtor ? separtor : "") : "\n");
+	va_end(ert);
+}
