2c42fe3d43ab1caf5f7b4666623e39aa361e13bb
aymnn <ayman.belahsen.4@gmail.com>
2023-10-19 17:24:05 -0700

    Linked list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+
+/**
+ * _strlen - prints the length of a str
+ *
+ * @s: the str to check the length from
+ *
+ * Return: length of str
+*/
+
+int _strlen(char *s)
+{
+	int e = 0;
+
+	if (!s)
+		return (0);
+	while (*s++)
+		e++;
+	return (e);
+}
+
+/**
+ * print_list - prints a singly linked list
+ *
+ * @h: ptr for the first node
+ *
+ * Return: size of list
+*/
+
+size_t print_list(const list_t *h)
+{
+	size_t e = 0;
+
+	while (h)
+	{
+		printf("[%d] %s\n", _strlen(h->str), ? h->str : "(nil)");
+		h = h->next;
+		e++;
+	}
+	return (e);
+}
