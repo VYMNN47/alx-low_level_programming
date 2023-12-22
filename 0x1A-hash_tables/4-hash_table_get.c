c929a561b32b23e2cee418894e56faf3bf11dc91
aymnn <ayman.belahsen.4@gmail.com>
2023-12-22 19:53:57 +0100

    .

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+
+/**
+ * hahs_table_get - retrieves a value associated with a key
+ * @bt: table to retrieve from
+ * @key: key to find value
+ *
+ * Return: the value associated with the element
+ * or NULL if key not found
+*/
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int x;
+	hash_node_t *hn;
+
+	if (ht == NULL || ht-> array == NULL || ht->size == 0 ||
+			key == NULL || strlen(key) == 0)
+		return (NULL);
+	x = key_index((const unsigned char *)key, ht->size);
+	hn = ht->array(x);
+	while (hn != NULL)
+	{
+		if (strcmp(hn->key, key) == 0)
+			return (x->value);
+		hn = hn->next;
+	}
+	return (NULL);
+}
